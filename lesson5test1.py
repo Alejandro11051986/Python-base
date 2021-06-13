bc1c12b79981445675565806d6e17e7f7f2c6d4e
Alejandro11051986 <82532281+Alejandro11051986@users.noreply.github.com>
2021-06-13 22:26:12 +0300

    Lesson5Homework

---
+++ lesson5test1.py
@@ -0,0 +1,23 @@
+#Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+#Об окончании ввода данных свидетельствует пустая строка.
+
+
+
+def get_data():
+    data_list = []
+    while True:
+        data = input("Введите данные: ")
+        if not data:
+            break
+        else:
+            data_list.append(data)
+    return data_list
+
+
+def make_txt():
+    with open("homework_1.txt", 'a', encoding="utf-8") as text_file:
+        for line in get_data():
+            text_file.write(line +"\n")
+
+
+make_txt()
